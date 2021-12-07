34376798dd01855e285330f515765d8240e64c46
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2021-12-07 12:29:40 -1000

    got single event page to load, need to work on styling

--- frontend/src/components/SingleEvent/index.js
+++ frontend/src/components/SingleEvent/index.js
@@ -1,24 +1,49 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import * as eventActions from '../../store/event';
 import './SingleEvent.css';
 
 function SingleEventPage() {
     const dispatch = useDispatch();
-    // const sessionUser = useSelector(state => state.session.user);
-    const eventObj = useSelector(state => state.event);
-    const event = Object.values(eventObj)[0];
-    const eventId = event?.id;
+    const { id } = useParams();
+    const sessionUser = useSelector(state => state.session.user);
+    const [event, setEvent] = useState('');
 
     useEffect(() => {
-        dispatch(eventActions.getSingleEvent(eventId));
-    }, [dispatch])
+        dispatch(eventActions.getSingleEvent(id)).then(res => setEvent(res));
+    }, [dispatch, id]);
+
+    const { User, body, title, time } = event;
+    const username = User?.username;
+    const date = new Date(time);
+    const day = date.toLocaleDateString();
+    let hours;
+    let ampm;
+    if (date.getHours() < 12) {
+        hours = date.getHours();
+        ampm = 'AM';
+    } else if (date.getHours() === 12) {
+        hours = date.getHours();
+        ampm = 'PM';
+    } else if (date.getHours() === 0) {
+        hours = 12;
+        ampm = 'AM'
+    } else {
+        hours = date.getHours() - 12;
+        ampm = 'PM';
+    }
+    const minutes = date.getMinutes();
 
     return (
-        <>
-        </>
+        <div className='single-event-page'>
+            <p className='created-by'>{username ? `Created by: ${username}` : null}</p>
+            <p className='event-title'>{title}</p>
+            <p className='event-body'>{body}</p>
+            <p className='event-time'>{`${day} ${hours}:${minutes} ${ampm}`}</p>
+        </div>
     )
 }
 
